c803826958517376cb79ceda30f328af90f46ffa
Hursh Gupta <hurshgupta1@gmail.com>
2024-05-08 18:52:10 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -62,7 +62,7 @@ export default function Navbar() {
             <Button
               variant="outline"
               size="icon"
-              className="shrink-0 md:hidden"
+              className="shrink-0 hidden md:hidden"
             >
               <Menu className="h-5 w-5" />
               <span className="sr-only">Toggle navigation menu</span>
